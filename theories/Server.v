a194086bee528143e0e3fbc99ee3670f66cebd8c
Yishuai Li <yishuai@upenn.edu>
2021-10-05 01:07:33 -0400

    Add server and observer

--- theories/Server.v
+++ theories/Server.v
@@ -1,23 +1,59 @@
+From ExtLib Require Export
+     Extras
+     RelDec.
+From Ceres Require Export
+     Ceres.
 From ITree Require Export
+     Exception
      ITree.
+From Coq Require Export
+     String.
 Export
+  FunNotation
   Monads
   ITreeNotations.
 Open Scope itree_scope.
+Open Scope string_scope.
 
 Section Server.
 
-Parameters Q A S : Type.
+Variables Q A S : Type.
+
+Hypothesis RelDec__A   : RelDec (@eq A).
+Hypothesis Serialize__A: Serialize A.
+Hypothesis Serialize__Q: Serialize Q.
 
 Variant serverE : Type -> Type :=
-  Server__Recv : serverE Q
-| Server__Send : A -> serverE unit.
+  Server__Recv : S -> serverE Q
+| Server__Exec : Q -> A -> serverE unit.
 
 Definition serverOf (step: Q -> state S A) : S -> itree serverE void :=
   rec (fun s =>
-         q <- trigger Server__Recv;;
+         q <- embed Server__Recv s;;
          let (s', a) := step q s in
-         embed Server__Send a;;
+         embed Server__Exec q a;;
          call s').
+
+Variant observerE : Type -> Type :=
+  Observer__FromServer : Q -> observerE A
+| Observer__FromClient : S -> observerE Q.
+
+Notation failureE := (exceptE string).
+
+Class Is__oE E `{observerE -< E} `{failureE -< E}.
+
+Definition observe {E} `{Is__oE E} (m: itree serverE void) : itree E void :=
+  interp
+    (fun _ e =>
+       match e in serverE Y return _ Y with
+       | Server__Recv s => embed Observer__FromClient s
+       | Server__Exec q a =>
+         a' <- embed Observer__FromServer q;;
+         if a' ?[ eq ] a
+         then Ret tt
+         else throw $ "Upon " ++ to_string q ++
+                    ", expect " ++ to_string a ++
+                    ", but observed " ++ to_string a'
+       end) m.
 
 End Server.
--- theories/Syntax.v
+++ theories/Syntax.v
@@ -1,5 +1,10 @@
 From FileSync Require Export
      File.
+From ExtLib Require Export
+     List
+     RelDec.
+From Ceres Require Export
+     Ceres.
 
 Variant F :=
   Fls    (p: path)
@@ -21,3 +26,42 @@ Variant Q :=
 | QSync.
 
 Definition S : Type := node * node * node.
+
+Instance RelDec_A : RelDec (@eq A) :=
+  { rel_dec a b :=
+      match a, b with
+      | Als   x, Als   y
+      | Aread x, Aread y => x ?[ eq ] y
+      | Ayes   , Ayes
+      | Ano    , Ano     => true
+      | _      , _       => false
+      end
+  }.
+
+Open Scope sexp_scope.
+
+Instance Serialize__A: Serialize A :=
+  fun a => match a with
+         | Als   l => to_sexp l
+         | Aread c => to_sexp c
+         | Ayes    => Atom "yes"
+         | Ano     => Atom "no"
+         end.
+
+Instance Serialize__F: Serialize F :=
+  fun f => match f with
+         | Fls    p   => [Atom "ls"   ; to_sexp p]
+         | Fread  p   => [Atom "read" ; to_sexp p]
+         | Fwrite p c => [Atom "write"; to_sexp p; to_sexp c]
+         | Fmkdir p   => [Atom "mkdir"; to_sexp p]
+         | Frm    p   => [Atom "rm"   ; to_sexp p]
+         end.
+
+Instance Serialize__R: Serialize R :=
+  fun r => Atom (if r is R1 then "R1" else "R2").
+
+Instance Serialize__Q: Serialize Q :=
+  fun q => if q is QFile r f then [to_sexp r; to_sexp f]
+         else Atom "Sync".
+
+Close Scope sexp_scope.
